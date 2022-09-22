504037973fa04a087e407ecf4d2744419b54e521
Nicole Miguel <nicolemonteiromiguel@gmail.com>
2022-09-22 18:39:09 -0300

    desenvolvendo sistema horus

---
+++ Js/horus.js
@@ -0,0 +1,9 @@
+function validarEmail () {
+    var email = document.getElementById("email_id");
+
+    if(email == " " || email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 )
+     {
+        alert( "Por favor, informe um e-mail válido!" );
+        return false;
+     }
+}
